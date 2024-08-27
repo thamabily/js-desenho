a58f0da1be037ac323e3b9b6eb09beab6b694b2e
thamabily <00001131530718sp@al.educacao.sp.gov.br>
2024-08-27 13:15:01 -0300

    adicionando css e js

---
+++ sketch.js
@@ -0,0 +1,133 @@
+function setup() {
+    createCanvas(400, 400);
+  }
+  
+  function draw() {
+    background(220);
+  }
+  
+  function setup() {
+    createCanvas(600, 600);
+  }
+  
+  function draw() {
+    background("white");
+  }
+  
+  function setup() {
+    createCanvas(600, 600);
+  }
+  
+  function draw() {
+    background("white");
+      rect(0, 0, 100, 150)
+  }
+  
+  function setup() {
+    createCanvas(600, 600);
+  }
+  
+  function draw() {
+    background("white");
+  
+      fill("red")
+      rect(0, 0, 100, 150)
+  }
+  
+  function setup() {
+    createCanvas(600, 600);
+  }
+  
+  function draw() {
+    background("white");
+  
+      stroke("blue")
+      fill("red")
+      rect(0, 0, 100, 150)
+  }
+  
+  function setup() {
+    createCanvas(600, 600);
+  }
+  
+  function draw() {
+    background("white");
+  
+    stroke("blue")
+    fill("red")
+    rect(100, 250, 100, 150)
+  }
+  
+  function setup() {
+    createCanvas(600, 600);
+  }
+  
+  function draw() {
+    background("white");
+  
+    stroke("blue")
+    fill("red")
+    rect(mouseX, mouseY, 20, 35)
+  }
+  
+  function setup() {
+    createCanvas(600, 600);
+  }
+  
+  function draw() {
+    background("white");
+  
+    stroke("blue")
+    fill("red")
+    
+    console.log(mouseIsPressed)
+    rect(mouseX, mouseY, 20, 35)
+  }
+  
+  function setup() {
+    createCanvas(600, 600);
+  }
+  
+  function draw() {
+    background("white");
+  
+    stroke("blue")
+    fill("red")
+    
+    // console.log(mouseIsPressed)
+    rect(mouseX, mouseY, 20, 35)
+  }
+  
+  function setup() {
+    createCanvas(600, 600);
+  }
+  
+  function draw() {
+    background("white");
+  
+    stroke("blue")
+    fill("red")
+    
+    // console.log(mouseIsPressed)
+    
+    if(mouseIsPressed) {
+        rect(mouseX, mouseY, 20, 35)
+    } 
+  }
+  
+  function setup() {
+    createCanvas(600, 600);
+    background("white");
+  }
+  
+  function draw() {
+    stroke("blue");
+    fill("red");
+  
+    // console.log(mouseIsPressed)
+  
+    if (mouseIsPressed) {
+      rect(mouseX, mouseY, 20, 35);
+    }
+  }
+  
